c3c63c71cadf0da0efadcc2850c69b281e460abd
CodingEvangelist <gnconsultinggroup@hotmail.de>
2015-02-07 15:36:09 +0100

    minimal change.
    Signed-off-by: CodingEvangelist <gnconsultinggroup@hotmail.de>

--- GeekQuiz/Models/AccountViewModels.cs
+++ GeekQuiz/Models/AccountViewModels.cs
@@ -7,7 +7,9 @@ namespace GeekQuiz.Models
     {
         [Required]
         [Display(Name = "Email")]
-        public string Email { get; set; }
+        public string Email { get; set; 
+        
+        }
     }
 
     public class ExternalLoginListViewModel
