4b9b6ac18adaa8fb3b2b8281181dadd65cc3b9cb
CodingEvangelist <gnconsultinggroup@hotmail.de>
2015-02-07 14:53:31 +0100

    local change

--- GeekQuiz/Models/IdentityModels.cs
+++ GeekQuiz/Models/IdentityModels.cs
@@ -15,6 +15,7 @@ namespace GeekQuiz.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
+
         }
     }
 
